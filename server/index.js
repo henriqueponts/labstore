5e8c1b1286e25ed8d738cbe948bb341026e65ca8
henriqueponts <henriquearaujopontes@gmail.com>
2025-07-03 15:12:05 -0300

    Nova autenticão
    Base para autenticação

---
+++ server/index.js
@@ -0,0 +1,9 @@
+  //"type": "commonjs" caso de BO no jSON
+
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
